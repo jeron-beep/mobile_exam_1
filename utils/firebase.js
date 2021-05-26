560ca919767a62ed0ebf71b161be45e8aa947014
Marc Angelo Chiapco <66046436+inn0ichi@users.noreply.github.com>
2021-05-26 12:05:16 +0800

    init

---
+++ utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCF93trrfIMMWJT-Sf9lX5pbQWwrUXfGmY",
+    authDomain: "bsit-act-4.firebaseapp.com",
+    projectId: "bsit-act-4",
+    storageBucket: "bsit-act-4.appspot.com",
+    messagingSenderId: "789359213036",
+    appId: "1:789359213036:web:e47419eb0faafedd92e8c9",
+  };
+  
+  firebase.initializeApp(firebaseConfig);
